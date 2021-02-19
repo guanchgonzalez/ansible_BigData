3634107ef547d5e55e0a7c10ea7e7664cc20c8d3
guanchgonzalez <guanchgonzalez@gmail.com>
2021-02-19 17:11:40 +0000

    'kafka' role completed, but not operational.

--- roles/spark/files/TDM_P2_anexo_2_WordCount.py
+++ roles/spark/files/TDM_P2_anexo_2_WordCount.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 
-# import required Python dependencies
-#import findspark
-#findspark.init()
+# Imports
 from pyspark import SparkContext, SparkConf
 
-# instantiate the Spark Context
-#conf = SparkConf() \
-#  .setMaster("spark://10.6.xxx.xxx:7077") \
+# Instantiate the Spark Context
 conf = SparkConf() \
-  .setAppName("Word counter")
+       .setAppName("Word counter")
 sc = SparkContext(conf=conf)
 sc
 
-# read text file
+# Read text file
 f = sc.textFile("/var/log/dnf.log")
 
-# define map and reduce transformations
+# Define map and reduce transformations
 wc = f.flatMap(lambda line: line.split(' ')) \
-  .map(lambda word: (word, 1)) \
-  .reduceByKey(lambda a,b: a+b)
+     .map(lambda word: (word, 1)) \
+     .reduceByKey(lambda a,b: a+b)
 
-# trigger action to execute transformations and collect results
+# Trigger action to execute transformations and collect results
 wc.collect()
 
 sc.stop()
